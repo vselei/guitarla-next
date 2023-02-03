e5e7c73927fe2483a55636d81e1fb5deaf656da4
Victor Seleimend <vsennaselei@gmail.com>
2023-02-03 13:12:36 -0300

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 const Footer = () => {
-  return <h1>Footer</h1>
-}
+  return (
+    <footer>
+      <div className="container">
+        <nav className='navigation'>
+          <Link href="/">Home</Link>
+          <Link href="/about-us">Sobre Nós</Link>
+          <Link href="/store">Loja</Link>
+          <Link href="/blog">Blog</Link>
+        </nav>
+        <p>Todos os direitos reservados {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
