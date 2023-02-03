dad222720da83243e69dd94a4b8654d90488d970
Victor Seleimend <vsennaselei@gmail.com>
2023-02-03 12:48:23 -0300

    Estilizando o header

--- components/header.js
+++ components/header.js
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import styles from '@/styles/header.module.css';
+
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <Image src='/img/logo.svg' width={300} height="40" alt="logo" />
-        <nav>
-          <Link href='/'>Home</Link> 
-          <Link href='/about-us'>Sobre Nós</Link> 
-          <Link href='/store'>Loja</Link> 
-          <Link href='/blog'>Blog</Link> 
+    <header className={styles.header}>
+      <div className={`container ${styles.bar}`}>
+        <Image src="/img/logo.svg" width={300} height="40" alt="logo" />
+        <nav className={styles.navigation}>
+          <Link href="/">Home</Link>
+          <Link href="/about-us">Sobre Nós</Link>
+          <Link href="/store">Loja</Link>
+          <Link href="/blog">Blog</Link>
         </nav>
       </div>
     </header>
