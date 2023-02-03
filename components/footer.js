c848a993141f0664b646c8954dcb1a8890cd8b47
Victor Seleimend <vsennaselei@gmail.com>
2023-02-03 13:20:22 -0300

    estilização do footer

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,20 @@
 import Link from 'next/link';
 
+import styles from '@/styles/footer.module.css';
+
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <nav className='navigation'>
+    <footer className={styles.footer}>
+      <div className={`container ${styles.content}`}>
+        <nav className={styles.navigation}>
           <Link href="/">Home</Link>
           <Link href="/about-us">Sobre Nós</Link>
           <Link href="/store">Loja</Link>
           <Link href="/blog">Blog</Link>
         </nav>
-        <p>Todos os direitos reservados {new Date().getFullYear()}</p>
+        <p className={styles.copyright}>
+          Todos os direitos reservados {new Date().getFullYear()}
+        </p>
       </div>
     </footer>
   );
