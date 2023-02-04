57c720307ccc32e28aae60461315fab55a201921
Victor Seleimend <vsennaselei@gmail.com>
2023-02-04 10:50:22 -0300

    estilização do carrinho

--- pages/cart.js
+++ pages/cart.js
@@ -7,11 +7,11 @@ const Cart = () => {
     <Layout title="Carrinho">
       <main className="container">
         <h1 className="heading">Carrinho</h1>
-        <div>
-          <div>
+        <div className={styles.content}>
+          <div className={styles.cart}>
             <h2>Itens</h2>
           </div>
-          <aside>
+          <aside className={styles.summary}>
             <h3>Resumo do Pedido</h3>
             <p>Total a Pagar: $</p>
           </aside>
